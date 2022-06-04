62d84f0d43d1fa593f62b277705ef1ad6532a260
syaza <syazlaikha7@gmail.com>
2022-06-04 21:47:28 +0800

    revoke
    revoke

---
+++ manager/includes/header.php
@@ -0,0 +1,8 @@
+<div class="brand clearfix">
+           <!-- Logo -->
+        <div id="logo" style="padding: 4px 15px;;">
+            <img src="img/logofkreser.png" />
+        </div>
+
+
+    </div>
